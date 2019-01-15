57198576c92e53a35ca69ec012a1659c99dda7b7
Williams Reyes <willianreyes52@hotmail.com>
2019-01-14 22:00:41 -0500

    url db heroku-postgres

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports ={
     development: {
         client: 'postgresql',
-        connection: 'postgresql://postgres:postgres@localhost:5432/clase'
+        connection: 'postgres://pfhtmswklaodgk:e37ac1cc2de0861ffa9a66f4a13500e84b21824e3d1b28a2414dddfebccca3d8@ec2-23-21-86-22.compute-1.amazonaws.com:5432/d6vlj0j8n9i0ko'
     },
     
     production:{
